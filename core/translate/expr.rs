7023ffc215511fc56e99174f8755b6aa434eb6f7
Jussi Saurio <jussi.saurio@gmail.com>
2025-02-16 08:59:07 +0200

    expr.rs: FunctionCall: call translate_expr() from translate_condition_expr()

--- core/translate/expr.rs
+++ core/translate/expr.rs
@@ -272,7 +272,7 @@ pub fn translate_condition_expr(
                 }
             }
         }
-        ast::Expr::Literal(_) | ast::Expr::Cast { .. } => {
+        ast::Expr::Literal(_) | ast::Expr::Cast { .. } | ast::Expr::FunctionCall { .. }  => {
             let reg = program.alloc_register();
             translate_expr(program, Some(referenced_tables), expr, reg, resolver)?;
             emit_cond_jump(program, condition_metadata, reg);
