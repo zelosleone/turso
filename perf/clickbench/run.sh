32e73d7fb5fb36600635526a32080754b4bd2300
Henrik Ingo <henrik@nyrk.io>
2025-02-18 02:52:46 +0200

    Nyrkiö: Analyze limbo and sqlite3 clickbench results separately

--- perf/clickbench/run.sh
+++ perf/clickbench/run.sh
@@ -30,15 +30,17 @@ count=1;
 # Run the queries, skipping any that are commented out
 # Between each invocation to Limbo/Sqlite, purge the caches
 grep -v '^--' "$CLICKBENCH_DIR/queries.sql" | while read -r query; do
-    echo "$count $query" | tee -a "${REPO_ROOT}/clickbench-limbo.txt" | tee -a "${REPO_ROOT}/clickbench-sqlite3.txt"
+    echo "$count $query"
+    ((echo "$count $query") 2>&1) | tee -a clickbench-limbo.txt > /dev/null
+    ((echo "$count $query") 2>&1) | tee -a clickbench-sqlite3.txt >/dev/null
     for _ in $(seq 1 $TRIES); do
         clear_caches
         echo "----limbo----"
-        time "$RELEASE_BUILD_DIR/limbo" --quiet "$CLICKBENCH_DIR/mydb" <<< "${query}"  | tee -a "${REPO_ROOT}/clickbench-limbo.txt"
+        ((time "$RELEASE_BUILD_DIR/limbo" --quiet "$CLICKBENCH_DIR/mydb" <<< "${query}") 2>&1) | tee -a clickbench-limbo.txt
         clear_caches
         echo
         echo "----sqlite----"
-        time sqlite3 "$CLICKBENCH_DIR/mydb" <<< "${query}" | tee -a "${REPO_ROOT}/clickbench-sqlite3.txt"
+        ((time sqlite3 "$CLICKBENCH_DIR/mydb" <<< "${query}") 2>&1) | tee -a clickbench-sqlite3.txt
     done;
     count=$(($count+1))
 done;
