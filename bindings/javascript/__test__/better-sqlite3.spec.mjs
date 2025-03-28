94262e466033d5011a6d375d18ba6879c6bdf96c
Pekka Enberg <penberg@iki.fi>
2025-03-28 10:52:11 +0200

    bindings/javascript: Fix Statement.get() implementation

--- bindings/javascript/__test__/better-sqlite3.spec.mjs
+++ bindings/javascript/__test__/better-sqlite3.spec.mjs
@@ -7,11 +7,21 @@ test("Open in-memory database", async (t) => {
     t.is(db.memory, true);
 });
 
-test("Statement.get()", async (t) => {
+
+test("Statement.get() returns data", async (t) => {
     const [db] = await connect(":memory:");
     const stmt = db.prepare("SELECT 1");
     const result = stmt.get();
     t.is(result["1"], 1);
+    const result2 = stmt.get();
+    t.is(result2["1"], 1);
+});
+
+test("Statement.get() returns undefined when no data", async (t) => {
+    const [db] = await connect(":memory:");
+    const stmt = db.prepare("SELECT 1 WHERE 1 = 2");
+    const result = stmt.get();
+    t.is(result, undefined);
 });
 
 const connect = async (path) => {
