4dde356d9763b544472a0811f5ca45e86460e3d1
Jussi Saurio <jussi.saurio@gmail.com>
2025-05-12 11:22:00 +0300

    AccessMethod: simplify

--- core/translate/optimizer/access_method.rs
+++ core/translate/optimizer/access_method.rs
@@ -23,51 +23,6 @@ pub struct AccessMethod<'a> {
     pub kind: AccessMethodKind<'a>,
 }
 
-impl<'a> AccessMethod<'a> {
-    pub fn set_iter_dir(&mut self, new_dir: IterationDirection) {
-        match &mut self.kind {
-            AccessMethodKind::Scan { iter_dir, .. } => *iter_dir = new_dir,
-            AccessMethodKind::Search { iter_dir, .. } => *iter_dir = new_dir,
-        }
-    }
-
-    pub fn set_constraint_refs(
-        &mut self,
-        new_index: Option<Arc<Index>>,
-        new_constraint_refs: &'a [ConstraintRef],
-    ) {
-        match (&mut self.kind, new_constraint_refs.is_empty()) {
-            (
-                AccessMethodKind::Search {
-                    constraint_refs,
-                    index,
-                    ..
-                },
-                false,
-            ) => {
-                *constraint_refs = new_constraint_refs;
-                *index = new_index;
-            }
-            (AccessMethodKind::Search { iter_dir, .. }, true) => {
-                self.kind = AccessMethodKind::Scan {
-                    index: new_index,
-                    iter_dir: *iter_dir,
-                };
-            }
-            (AccessMethodKind::Scan { iter_dir, .. }, false) => {
-                self.kind = AccessMethodKind::Search {
-                    index: new_index,
-                    iter_dir: *iter_dir,
-                    constraint_refs: new_constraint_refs,
-                };
-            }
-            (AccessMethodKind::Scan { index, .. }, true) => {
-                *index = new_index;
-            }
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 /// Represents the kind of access method.
 pub enum AccessMethodKind<'a> {
@@ -130,7 +85,8 @@ pub fn find_best_access_method_for_join_order<'a>(
         );
 
         // All other things being equal, prefer an access method that satisfies the order target.
-        let order_satisfiability_bonus = if let Some(order_target) = maybe_order_target {
+        let (iter_dir, order_satisfiability_bonus) = if let Some(order_target) = maybe_order_target
+        {
             // If the index delivers rows in the same direction (or the exact reverse direction) as the order target, then it
             // satisfies the order target.
             let mut all_same_direction = true;
@@ -163,62 +119,38 @@ pub fn find_best_access_method_for_join_order<'a>(
                 }
             }
             if all_same_direction || all_opposite_direction {
-                Cost(1.0)
+                (
+                    if all_same_direction {
+                        IterationDirection::Forwards
+                    } else {
+                        IterationDirection::Backwards
+                    },
+                    Cost(1.0),
+                )
             } else {
-                Cost(0.0)
+                (IterationDirection::Forwards, Cost(0.0))
             }
         } else {
-            Cost(0.0)
+            (IterationDirection::Forwards, Cost(0.0))
         };
         if cost < best_access_method.cost + order_satisfiability_bonus {
-            best_access_method.cost = cost;
-            best_access_method
-                .set_constraint_refs(candidate.index.clone(), &usable_constraint_refs);
+            best_access_method = AccessMethod {
+                cost,
+                kind: if usable_constraint_refs.is_empty() {
+                    AccessMethodKind::Scan {
+                        index: candidate.index.clone(),
+                        iter_dir,
+                    }
+                } else {
+                    AccessMethodKind::Search {
+                        index: candidate.index.clone(),
+                        iter_dir,
+                        constraint_refs: &usable_constraint_refs,
+                    }
+                },
+            };
         }
     }
-
-    let iter_dir = if let Some(order_target) = maybe_order_target {
-        // if index columns match the order target columns in the exact reverse directions, then we should use IterationDirection::Backwards
-        let index = match &best_access_method.kind {
-            AccessMethodKind::Scan { index, .. } => index.as_ref(),
-            AccessMethodKind::Search { index, .. } => index.as_ref(),
-        };
-        let mut should_use_backwards = true;
-        let num_cols = index.map_or(1, |i| i.columns.len());
-        for i in 0..order_target.0.len().min(num_cols) {
-            let correct_table = order_target.0[i].table_no == table_no;
-            let correct_column = {
-                match index {
-                    Some(index) => index.columns[i].pos_in_table == order_target.0[i].column_no,
-                    None => {
-                        rowid_column_idx.map_or(false, |idx| idx == order_target.0[i].column_no)
-                    }
-                }
-            };
-            if !correct_table || !correct_column {
-                should_use_backwards = false;
-                break;
-            }
-            let correct_order = {
-                match index {
-                    Some(index) => order_target.0[i].order == index.columns[i].order,
-                    None => order_target.0[i].order == SortOrder::Asc,
-                }
-            };
-            if correct_order {
-                should_use_backwards = false;
-                break;
-            }
-        }
-        if should_use_backwards {
-            IterationDirection::Backwards
-        } else {
-            IterationDirection::Forwards
-        }
-    } else {
-        IterationDirection::Forwards
-    };
-    best_access_method.set_iter_dir(iter_dir);
 
     Ok(best_access_method)
 }
