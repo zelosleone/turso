5477d0a4f5ae6274e26a014d57a2b453bb16347c
Pekka Enberg <penberg@iki.fi>
2025-06-16 09:10:29 +0300

    sqlite3: Clean up sqlite3_safety_check_sick_or_ok() function
    Underscore in parameter or variable name indicates it's not used. Also,
    drop useless `crate` qualifier from constants.

--- sqlite3/src/lib.rs
+++ sqlite3/src/lib.rs
@@ -1167,11 +1167,11 @@ pub unsafe extern "C" fn libsql_wal_get_frame(
     }
 }
 
-fn sqlite3_safety_check_sick_or_ok(_db: &sqlite3) -> bool {
-    match _db.e_open_state {
-        crate::SQLITE_STATE_SICK | crate::SQLITE_STATE_OPEN | crate::SQLITE_STATE_BUSY => true,
+fn sqlite3_safety_check_sick_or_ok(db: &sqlite3) -> bool {
+    match db.e_open_state {
+        SQLITE_STATE_SICK | SQLITE_STATE_OPEN | SQLITE_STATE_BUSY => true,
         _ => {
-            eprintln!("Invalid database state: {}", _db.e_open_state);
+            eprintln!("Invalid database state: {}", db.e_open_state);
             false
         }
     }
