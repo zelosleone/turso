6c8aa4e04312987faa6b0bdfc87333c1686d0c52
Pekka Enberg <penberg@iki.fi>
2025-06-24 14:16:09 +0300

    antithesis: Run experiment for 60 minutes

--- scripts/antithesis/launch.sh
+++ scripts/antithesis/launch.sh
@@ -3,7 +3,7 @@
 curl --fail -u "$ANTITHESIS_USER:$ANTITHESIS_PASSWD" \
   -X POST https://$ANTITHESIS_TENANT.antithesis.com/api/v1/launch/basic_test \
   -d "{\"params\": { \"antithesis.description\":\"basic_test on main\",
-      \"antithesis.duration\":\"1\",
+      \"antithesis.duration\":\"60\",
       \"antithesis.config_image\":\"$ANTITHESIS_DOCKER_REPO/limbo-config:antithesis-latest\",
       \"antithesis.images\":\"$ANTITHESIS_DOCKER_REPO/limbo-workload:antithesis-latest\",
       \"antithesis.report.recipients\":\"$ANTITHESIS_EMAIL\"
