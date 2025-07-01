578c7ae53aa3a3842f871ab9b4d7bd1b9e26f455
Andika Tanuwijaya <holy.arc.64633@gmail.com>
2025-07-02 00:39:36 +0700

    example

---
+++ bindings/dart/example/lib/infra/libsql_task_repository.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:turso_dart/turso_dart.dart';
+import 'package:turso_dart_example/features/task/models/task.dart';
+import 'package:turso_dart_example/features/task/repositories/repositories.dart';
+import 'package:watcher/watcher.dart';
+
+class LibsqlTaskRepository extends TaskRepository {
+  final LibsqlClient _client;
+
+  LibsqlTaskRepository(this._client);
+
+  @override
+  Future<void> addTask(Task task) async {
+    await _client.execute(
+      "insert into tasks (title, description, completed) values (?, ?, ?)",
+      positional: [task.title, task.description, task.completed ? 1 : 0],
+    );
+  }
+
+  @override
+  Future<void> deleteTask(int id) async {
+    await _client.execute("delete from tasks where id = ?", positional: [id]);
+  }
+
+  @override
+  Future<List<Task>> getTasks() async {
+    return _client
+        .query("select * from tasks")
+        .then(
+          (value) => value
+              .map(
+                (row) => Task(
+                  id: row["id"],
+                  title: row["title"],
+                  description: row["description"],
+                  completed: row["completed"] == 1,
+                ),
+              )
+              .toList(),
+        );
+  }
+
+  @override
+  Future<void> markTasksAsCompleted(List<int> ids) async {
+    await _client.execute(
+      "update tasks set completed = 1 where id in (${ids.join(",")})",
+    );
+  }
+
+  @override
+  Future<Stream?> replicaChanges() async {
+    if (_client.url == ":memory:") return null;
+    final dir = await getApplicationCacheDirectory();
+    return DirectoryWatcher(dir.path).events;
+  }
+}
--- bindings/dart/example/lib/main.dart
+++ bindings/dart/example/lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:turso_dart/turso_dart.dart';
+import 'package:turso_dart_example/bootstrap.dart';
+import 'package:turso_dart_example/features/task/repositories/task_repository.dart';
+import 'package:turso_dart_example/features/task/task_list.dart';
+import 'package:turso_dart_example/infra/libsql_task_repository.dart';
+
+late LibsqlClient memoryClient;
+late LibsqlClient localClient;
 
 Future<void> main() async {
-  final c = LibsqlClient.memory();
-  await c.connect();
-  final res = await c.query("SELECT 1");
-  print(res);
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationCacheDirectory();
+  await dir.delete(recursive: true);
+  await dir.create(recursive: true);
+
+  // memoryClient = LibsqlClient(":memory:");
+  memoryClient = LibsqlClient.memory();
+
+  // localClient = LibsqlClient("${dir.path}/local.db");
+  localClient = LibsqlClient.local("${dir.path}/local.db");
+
+  await bootstrapDatabase(memoryClient);
+  await bootstrapDatabase(localClient);
+
   runApp(const MyApp());
 }
 
@@ -16,7 +36,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        appBar: AppBar(title: const Text('Libsql Dart Example')),
+        body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Builder(
+            builder: (context) {
+              return Center(
+                child: Column(
+                  spacing: 16,
+                  children: [
+                    FilledButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Provider<TaskRepository>(
+                              create: (context) =>
+                                  LibsqlTaskRepository(memoryClient),
+                              child: const TaskList(),
+                            ),
+                          ),
+                        );
+                      },
+                      child: const Text("Memory"),
+                    ),
+                    FilledButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Provider<TaskRepository>(
+                              create: (context) =>
+                                  LibsqlTaskRepository(localClient),
+                              child: const TaskList(),
+                            ),
+                          ),
+                        );
+                      },
+                      child: const Text("Local"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
