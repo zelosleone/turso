9e92325bada18329378e7dd5cfb53ba8ccde4c9b
Pekka Enberg <penberg@iki.fi>
2025-07-03 15:58:49 +0300

    antithesis: Make experiments run for longer
    ...increases probability of Antithesis finding interesting things.

--- scripts/antithesis/launch.sh
+++ scripts/antithesis/launch.sh
@@ -3,7 +3,7 @@
 curl --fail -u "$ANTITHESIS_USER:$ANTITHESIS_PASSWD" \
   -X POST https://$ANTITHESIS_TENANT.antithesis.com/api/v1/launch/limbo \
   -d "{\"params\": { \"antithesis.description\":\"basic_test on main\",
-      \"custom.duration\":\"1\",
+      \"custom.duration\":\"4\",
       \"antithesis.config_image\":\"$ANTITHESIS_DOCKER_REPO/limbo-config:antithesis-latest\",
       \"antithesis.images\":\"$ANTITHESIS_DOCKER_REPO/limbo-workload:antithesis-latest\",
       \"antithesis.report.recipients\":\"$ANTITHESIS_EMAIL\"
