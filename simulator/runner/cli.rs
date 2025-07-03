5f9abb62c40b0d06753856212dfd36d7966a85c3
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-07-03 15:12:32 -0300

    enable faulty query

--- simulator/runner/cli.rs
+++ simulator/runner/cli.rs
@@ -84,7 +84,7 @@ pub struct SimulatorCLI {
     pub disable_select_optimizer: bool,
     #[clap(long, help = "disable FsyncNoWait Property", default_value_t = true)]
     pub disable_fsync_no_wait: bool,
-    #[clap(long, help = "disable FaultyQuery Property", default_value_t = true)]
+    #[clap(long, help = "disable FaultyQuery Property", default_value_t = false)]
     pub disable_faulty_query: bool,
     #[clap(long, help = "disable Reopen-Database fault", default_value_t = false)]
     pub disable_reopen_database: bool,
