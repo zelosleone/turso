4b6b2c9b00c6ae21fb527724e35f78be8e67cc80
김선우 <seonwoo960000@toss.im>
2025-07-06 17:10:04 +0900

    nit

--- bindings/java/src/main/java/tech/turso/jdbc4/JDBC4Connection.java
+++ bindings/java/src/main/java/tech/turso/jdbc4/JDBC4Connection.java
@@ -24,7 +24,7 @@ public final class JDBC4Connection implements Connection {
   }
 
   public TursoStatement prepare(String sql) throws SQLException {
-    final TursoStatement statement =  connection.prepare(sql);
+    final TursoStatement statement = connection.prepare(sql);
     statement.initializeColumnMetadata();
     return statement;
   }
