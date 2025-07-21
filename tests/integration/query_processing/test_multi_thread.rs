c55cb74dc8f4c2159f9d93e55c0dd3a3b05b0450
Pere Diaz Bou <pere-altea@hotmail.com>
2025-07-21 11:06:31 +0200

    simple write multi threaded test

--- tests/integration/query_processing/test_multi_thread.rs
+++ tests/integration/query_processing/test_multi_thread.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::common::TempDatabase;
 
 #[test]
@@ -24,4 +26,40 @@ fn test_schema_change() {
         }
     };
     println!("{:?} {:?}", row.get_value(0), row.get_value(1));
+}
+
+#[test]
+fn test_create_multiple_connections() -> anyhow::Result<()> {
+    let tries = 10;
+    for _ in 0..tries {
+        let tmp_db = Arc::new(TempDatabase::new_empty(false));
+        {
+            let conn = tmp_db.connect_limbo();
+            conn.execute("CREATE TABLE t(x)").unwrap();
+        }
+
+        let mut threads = Vec::new();
+        for i in 0..10 {
+            let tmp_db_ = tmp_db.clone();
+            threads.push(std::thread::spawn(move || {
+                let conn = tmp_db_.connect_limbo();
+                conn.execute(format!("INSERT INTO t VALUES ({i})").as_str())
+                    .unwrap();
+            }));
+        }
+        for thread in threads {
+            thread.join().unwrap();
+        }
+
+        let conn = tmp_db.connect_limbo();
+        let mut stmt = conn.prepare("SELECT * FROM t").unwrap();
+        let mut rows = Vec::new();
+        while matches!(stmt.step().unwrap(), turso_core::StepResult::Row) {
+            let row = stmt.row().unwrap();
+            rows.push(row.get::<i64>(0).unwrap());
+        }
+        rows.sort();
+        assert_eq!(rows, (0..10).collect::<Vec<_>>());
+    }
+    Ok(())
 }
