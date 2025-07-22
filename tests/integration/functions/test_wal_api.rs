2283a04aabb15039b5a0d7dbab47fde338025a89
Nikita Sivukhin <sivukhin@turso.tech>
2025-07-22 17:45:58 +0400

    add more tests

--- tests/integration/functions/test_wal_api.rs
+++ tests/integration/functions/test_wal_api.rs
@@ -55,3 +55,90 @@ fn test_wal_frame_transfer_no_schema_changes() {
         ]
     );
 }
+
+#[test]
+fn test_wal_frame_transfer_no_schema_changes_rollback() {
+    let db1 = TempDatabase::new_empty(false);
+    let conn1 = db1.connect_limbo();
+    let db2 = TempDatabase::new_empty(false);
+    let conn2 = db2.connect_limbo();
+    conn1
+        .execute("CREATE TABLE t(x INTEGER PRIMARY KEY, y)")
+        .unwrap();
+    conn2
+        .execute("CREATE TABLE t(x INTEGER PRIMARY KEY, y)")
+        .unwrap();
+    conn1
+        .execute("INSERT INTO t VALUES (1024, randomblob(4096 * 10))")
+        .unwrap();
+    assert_eq!(conn1.wal_frame_count().unwrap(), 14);
+    let mut frame = [0u8; 24 + 4096];
+    conn2.wal_insert_begin().unwrap();
+    for frame_id in 1..=(conn1.wal_frame_count().unwrap() as u32 - 1) {
+        let c = conn1.wal_get_frame(frame_id, &mut frame).unwrap();
+        db1.io.wait_for_completion(c).unwrap();
+        conn2.wal_insert_frame(frame_id, &frame).unwrap();
+    }
+    conn2.wal_insert_end().unwrap();
+    assert_eq!(conn2.wal_frame_count().unwrap(), 2);
+    assert_eq!(
+        limbo_exec_rows(&db2, &conn2, "SELECT x, length(y) FROM t"),
+        vec![] as Vec<Vec<rusqlite::types::Value>>
+    );
+    conn2.execute("CREATE TABLE q(x)").unwrap();
+    conn2
+        .execute("INSERT INTO q VALUES (randomblob(4096 * 10))")
+        .unwrap();
+    assert_eq!(
+        limbo_exec_rows(&db2, &conn2, "SELECT x, LENGTH(y) FROM t"),
+        vec![] as Vec<Vec<rusqlite::types::Value>>
+    );
+}
+
+#[test]
+fn test_wal_frame_conflict() {
+    let db1 = TempDatabase::new_empty(false);
+    let conn1 = db1.connect_limbo();
+    let db2 = TempDatabase::new_empty(false);
+    let conn2 = db2.connect_limbo();
+    conn1
+        .execute("CREATE TABLE t(x INTEGER PRIMARY KEY, y)")
+        .unwrap();
+    conn2
+        .execute("CREATE TABLE q(x INTEGER PRIMARY KEY, y)")
+        .unwrap();
+    assert_eq!(conn1.wal_frame_count().unwrap(), 2);
+    let mut frame = [0u8; 24 + 4096];
+    conn2.wal_insert_begin().unwrap();
+    let c = conn1.wal_get_frame(1, &mut frame).unwrap();
+    db1.io.wait_for_completion(c).unwrap();
+    assert!(conn2.wal_insert_frame(1, &frame).is_err());
+}
+
+#[test]
+fn test_wal_frame_far_away_write() {
+    let db1 = TempDatabase::new_empty(false);
+    let conn1 = db1.connect_limbo();
+    let db2 = TempDatabase::new_empty(false);
+    let conn2 = db2.connect_limbo();
+    conn1
+        .execute("CREATE TABLE t(x INTEGER PRIMARY KEY, y)")
+        .unwrap();
+    conn2
+        .execute("CREATE TABLE t(x INTEGER PRIMARY KEY, y)")
+        .unwrap();
+    conn1
+        .execute("INSERT INTO t VALUES (1024, randomblob(4096 * 10))")
+        .unwrap();
+    assert_eq!(conn1.wal_frame_count().unwrap(), 14);
+    let mut frame = [0u8; 24 + 4096];
+    conn2.wal_insert_begin().unwrap();
+
+    let c = conn1.wal_get_frame(3, &mut frame).unwrap();
+    db1.io.wait_for_completion(c).unwrap();
+    conn2.wal_insert_frame(3, &frame).unwrap();
+
+    let c = conn1.wal_get_frame(5, &mut frame).unwrap();
+    db1.io.wait_for_completion(c).unwrap();
+    assert!(conn2.wal_insert_frame(5, &frame).is_err());
+}
