f10723779d83de050c3757f8c1b8610633198979
Pekka Enberg <penberg@iki.fi>
2025-07-22 10:35:30 +0300

    bindings/javascript: Add Drizzle example

---
+++ bindings/javascript/examples/drizzle/example.js
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { sql } from "drizzle-orm";
+import Database from '@tursodatabase/turso';
+
+const sqlite = new Database('sqlite.db');
+const db = drizzle({ client: sqlite });
+const result = await db.all(sql`select 1`);
+console.log(result);
