701286080053fe762ecd9a04fb6b51b303af54b5
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-07-30 14:53:32 -0300

    create separate state machines file

--- core/storage/mod.rs
+++ core/storage/mod.rs
@@ -18,6 +18,7 @@ pub(crate) mod page_cache;
 #[allow(clippy::arc_with_non_send_sync)]
 pub(crate) mod pager;
 pub(crate) mod sqlite3_ondisk;
+mod state_machines;
 #[allow(clippy::arc_with_non_send_sync)]
 pub(crate) mod wal;
 
