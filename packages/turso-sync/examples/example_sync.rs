f3da874fd5d7d1fd44e44285151e25bbec837a25
Nikita Sivukhin <sivukhin@turso.tech>
2025-07-30 14:38:29 +0400

    fix clippy

--- packages/turso-sync/examples/example_sync.rs
+++ packages/turso-sync/examples/example_sync.rs
@@ -50,7 +50,7 @@ async fn main() {
             for i in 0..row.column_count() {
                 let value = row.get_value(i).unwrap();
                 match value {
-                    turso::Value::Null => values.push(format!("NULL")),
+                    turso::Value::Null => values.push("NULL".to_string()),
                     turso::Value::Integer(x) => values.push(format!("{x}")),
                     turso::Value::Real(x) => values.push(format!("{x}")),
                     turso::Value::Text(x) => values.push(format!("'{x}'")),
