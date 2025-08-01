1db0637a5eaaf301a12f3e6eb1498ef49cabb116
Pekka Enberg <penberg@iki.fi>
2025-08-01 16:55:04 +0300

    bindings/javascript: Improve benchmark

--- bindings/javascript/perf/perf-better-sqlite3.js
+++ bindings/javascript/perf/perf-better-sqlite3.js
@@ -8,12 +8,17 @@ db.exec("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)");
 db.exec("INSERT INTO users (id, name, email) VALUES (1, 'Alice', 'alice@example.org')");
 
 const stmt = db.prepare("SELECT * FROM users WHERE id = ?");
+const rawStmt = db.prepare("SELECT * FROM users WHERE id = ?").raw();
 
 group('Statement', () => {
-  bench('Statement.get() bind parameters', () => {
+  bench('Statement.get() with bind parameters [expanded]', () => {
     stmt.get(1);
   });
+  bench('Statement.get() with bind parameters [raw]', () => {
+    rawStmt.get(1);
+  });
 });
+
 
 await run({
   units: false,
