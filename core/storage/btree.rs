4b27cc0d4652abfa01c75410a144dcc3c486a34b
Jussi Saurio <jussi.saurio@gmail.com>
2025-08-07 09:54:33 +0300

    btree: add fast path version of cell_get_raw_region

--- core/storage/btree.rs
+++ core/storage/btree.rs
@@ -2795,10 +2795,21 @@ impl BTreeCursor {
                     {
                         let old_page = old_page.as_ref().unwrap().get();
                         let old_page_contents = old_page.get_contents();
+                        let page_type = old_page_contents.page_type();
+                        let max_local = payload_overflow_threshold_max(page_type, usable_space);
+                        let min_local = payload_overflow_threshold_min(page_type, usable_space);
+                        let cell_count = old_page_contents.cell_count();
                         debug_validate_cells!(&old_page_contents, usable_space as u16);
-                        for cell_idx in 0..old_page_contents.cell_count() {
-                            let (cell_start, cell_len) =
-                                old_page_contents.cell_get_raw_region(cell_idx, usable_space);
+                        for cell_idx in 0..cell_count {
+                            let (cell_start, cell_len) = old_page_contents
+                                ._cell_get_raw_region_faster(
+                                    cell_idx,
+                                    usable_space,
+                                    cell_count,
+                                    max_local,
+                                    min_local,
+                                    page_type,
+                                );
                             let buf = old_page_contents.as_ptr();
                             let cell_buf = &mut buf[cell_start..cell_start + cell_len];
                             // TODO(pere): make this reference and not copy
