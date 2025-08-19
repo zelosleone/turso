0138aade31bef9e3122adb62d6522a68b4117d4b
Levy A. <levyddsa@gmail.com>
2025-08-19 16:59:04 -0300

    tidy error reporting

--- parser/src/error.rs
+++ parser/src/error.rs
@@ -1,93 +1,55 @@
-use std::error;
-use std::fmt;
-
 use crate::token::TokenType;
 
 /// SQL lexer and parser errors
 #[non_exhaustive]
-#[derive(Clone, Debug, miette::Diagnostic)]
+#[derive(Clone, Debug, miette::Diagnostic, thiserror::Error)]
 #[diagnostic()]
 pub enum Error {
     /// Lexer error
+    #[error("unrecognized token at {0:?}")]
     UnrecognizedToken(#[label("here")] miette::SourceSpan),
     /// Missing quote or double-quote or backtick
+    #[error("non-terminated literal at {0:?}")]
     UnterminatedLiteral(#[label("here")] miette::SourceSpan),
     /// Missing `]`
+    #[error("non-terminated bracket at {0:?}")]
     UnterminatedBracket(#[label("here")] miette::SourceSpan),
     /// Missing `*/`
+    #[error("non-terminated block comment at {0:?}")]
     UnterminatedBlockComment(#[label("here")] miette::SourceSpan),
     /// Invalid parameter name
+    #[error("bad variable name at {0:?}")]
     BadVariableName(#[label("here")] miette::SourceSpan),
     /// Invalid number format
+    #[error("bad number at {0:?}")]
     BadNumber(#[label("here")] miette::SourceSpan),
     // Bad fractional part of a number
+    #[error("bad fractional part at {0:?}")]
     BadFractionalPart(#[label("here")] miette::SourceSpan),
     // Bad exponent part of a number
+    #[error("bad exponent part at {0:?}")]
     BadExponentPart(#[label("here")] miette::SourceSpan),
     /// Invalid or missing sign after `!`
+    #[error("expected = sign at {0:?}")]
     ExpectedEqualsSign(#[label("here")] miette::SourceSpan),
     /// Hexadecimal integer literals follow the C-language notation of "0x" or "0X" followed by hexadecimal digits.
+    #[error("malformed hex integer at {0:?}")]
     MalformedHexInteger(#[label("here")] miette::SourceSpan),
     // parse errors
     // Unexpected end of file
+    #[error("unexpected end of file")]
     ParseUnexpectedEOF,
     // Unexpected token
+    #[error("unexpected token at {parsed_offset:?}")]
+    #[diagnostic(help("expected {expected:?} but found {got:?}"))]
     ParseUnexpectedToken {
-        #[label("parsed to here")]
+        #[label("here")]
         parsed_offset: miette::SourceSpan,
 
         got: TokenType,
         expected: &'static [TokenType],
     },
     // Custom error message
+    #[error("{0}")]
     Custom(String),
 }
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Self::UnrecognizedToken(pos) => {
-                write!(f, "unrecognized token at {pos:?}")
-            }
-            Self::UnterminatedLiteral(pos) => {
-                write!(f, "non-terminated literal at {pos:?}")
-            }
-            Self::UnterminatedBracket(pos) => {
-                write!(f, "non-terminated bracket at {pos:?}")
-            }
-            Self::UnterminatedBlockComment(pos) => {
-                write!(f, "non-terminated block comment at {pos:?}")
-            }
-            Self::BadVariableName(pos) => write!(f, "bad variable name at {pos:?}"),
-            Self::BadNumber(pos) => write!(f, "bad number at {pos:?}"),
-            Self::BadFractionalPart(pos) => {
-                write!(f, "bad fractional part at {pos:?}")
-            }
-            Self::BadExponentPart(pos) => {
-                write!(f, "bad exponent part at {pos:?}")
-            }
-            Self::ExpectedEqualsSign(pos) => write!(f, "expected = sign at {pos:?}"),
-            Self::MalformedHexInteger(pos) => {
-                write!(f, "malformed hex integer at {pos:?}")
-            }
-            Self::ParseUnexpectedEOF => {
-                write!(f, "unexpected end of file")
-            }
-            Self::ParseUnexpectedToken {
-                parsed_offset,
-                got,
-                expected,
-            } => {
-                write!(
-                    f,
-                    "got unexpected token after parsing to offset {parsed_offset:?}: expected {expected:?}, found {got}",
-                )
-            }
-            Self::Custom(ref s) => {
-                write!(f, "custom error: {s}")
-            }
-        }
-    }
-}
-
-impl error::Error for Error {}
