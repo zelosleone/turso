918c2a3f690a6a3a3236f3f542324ae46b26c1f3
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-08-26 16:00:11 -0300

    extend latency profile + impl Default manually

--- simulator/profiles/io.rs
+++ simulator/profiles/io.rs
@@ -1,9 +1,36 @@
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct IOProfile {
     enable: bool,
+    latency: LatencyProfile,
 }
 
-#[derive(Debug, Default, Clone)]
+impl Default for IOProfile {
+    fn default() -> Self {
+        Self {
+            enable: true,
+            latency: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct LatencyProfile {
     enable: bool,
+    /// Added IO latency probability
+    latency_probability: usize,
+    /// Minimum tick time in microseconds for simulated time
+    min_tick: u64,
+    /// Maximum tick time in microseconds for simulated time
+    max_tick: u64,
+}
+
+impl Default for LatencyProfile {
+    fn default() -> Self {
+        Self {
+            enable: true,
+            latency_probability: 1,
+            min_tick: 1,
+            max_tick: 30,
+        }
+    }
 }
--- simulator/profiles/query.rs
+++ simulator/profiles/query.rs
@@ -1,29 +1,65 @@
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct CreateTableProfile {
     enable: bool,
 }
 
-#[derive(Debug, Default, Clone)]
+impl Default for CreateTableProfile {
+    fn default() -> Self {
+        Self { enable: true }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct CreateIndexProfile {
     enable: bool,
 }
 
-#[derive(Debug, Default, Clone)]
+impl Default for CreateIndexProfile {
+    fn default() -> Self {
+        Self { enable: true }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct InsertProfile {
     enable: bool,
 }
 
-#[derive(Debug, Default, Clone)]
+impl Default for InsertProfile {
+    fn default() -> Self {
+        Self { enable: true }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct UpdateProfile {
     enable: bool,
 }
 
-#[derive(Debug, Default, Clone)]
+impl Default for UpdateProfile {
+    fn default() -> Self {
+        Self { enable: true }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct DeleteProfile {
     enable: bool,
 }
 
-#[derive(Debug, Default, Clone)]
+impl Default for DeleteProfile {
+    fn default() -> Self {
+        Self { enable: true }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct DropTableProfile {
     enable: bool,
+}
+
+impl Default for DropTableProfile {
+    fn default() -> Self {
+        Self { enable: true }
+    }
 }
