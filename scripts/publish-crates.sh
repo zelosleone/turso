8f7e43b32bb643a9a85e598142d3174551007586
Pekka Enberg <penberg@iki.fi>
2025-09-02 14:21:04 +0300

    scripts/publish-crates.sh: Remove turso_sqlite3_parser package

--- scripts/publish-crates.sh
+++ scripts/publish-crates.sh
@@ -2,7 +2,6 @@
 
 cargo publish -p turso_macros
 cargo publish -p turso_ext
-cargo publish -p turso_sqlite3_parser
 cargo publish -p turso_parser
 cargo publish -p turso_core
 cargo publish -p turso
